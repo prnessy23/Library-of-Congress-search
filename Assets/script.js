8b4c783278e163552d7301475f94f9b41fb766a3
Michael Reyna <25.27.reyna@gmail.com>
2023-01-10 19:49:30 -0700

    added link to results.html

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,16 @@
-var form = document.getElementById("form");
 
+var form = document.getElementById("form");
+var url;
+var city = document.getElementById("city").value;
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    var city = document.getElementById("city").value;
+    city = document.getElementById("city").value;
     var option = document.getElementById("options").value;
+    url = `https://www.loc.gov/${option}/?q=${city}&fo=json`;
+
     console.log(option);
-    var url = `https://www.loc.gov/${option}/?q=${city}&fo=json`;
-    fetch(url)
-        .then(function (response){
-            return response.json();
-        })
-        .then(function (data){
-            console.log(data);
-        })
     location.replace('./results.html');
+    return url;
 })
+
